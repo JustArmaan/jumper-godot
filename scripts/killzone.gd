daadd989f74d821b24819817a4c26949b1fd8175
Armaan <singharmaanbrar@gmail.com>
2026-01-22 11:33:58 -0800

    feat: add death message and expand world

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,9 +1,11 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
+@onready var death_image: TextureRect = $CanvasLayer2/death_image
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died")
+	death_image.visible = true;
 	timer.start()
 
 func _on_timer_timeout() -> void:
