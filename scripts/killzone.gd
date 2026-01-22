ef8e15eb081c5a0d3e7aeca4e672eed7dd117022
Armaan <singharmaanbrar@gmail.com>
2026-01-22 12:41:18 -0800

    feat: add enemys and improve movement for both players and enemys

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -5,8 +5,11 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	print("You Died")
-	death_image.visible = true;
+	Engine.time_scale = 0.5
+	death_image.visible = true
 	timer.start()
+	body.get_node("CollisionShape2D").queue_free()
 
 func _on_timer_timeout() -> void:
+	Engine.time_scale = 1.0
 	get_tree().reload_current_scene()
